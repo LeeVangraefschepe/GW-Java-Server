9dc924f6f17613055a1d9e6b4a305b9ada3e93dc
LEEVGS <lee.vgs@hotmail.com>
2023-12-16 17:35:48 +0100

    Added console clearing before startup project

--- MainClass.java
+++ MainClass.java
@@ -4,6 +4,9 @@ public class MainClass
 {
     public static void main(String[] args)
     {
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+
         Scanner scanner = new Scanner(System.in);
 
          System.out.println("Started server on port: 1234");
