f3c221c3a2aa5d9100932c79f73ce1340e243b94
LEEVGS <lee.vgs@hotmail.com>
2023-12-17 22:23:01 +0100

    Added first test benchmark for Json

--- benchmark/BaseBenchmark.java
+++ benchmark/BaseBenchmark.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class BaseBenchmark
 {
-    protected short[][][] GenerateRandomChunk()
+    protected short[][][] GenerateRandomChunk(int height)
     {
         Random rand = new Random(); 
-        short[][][] blockData = new short[16][48][16];
+        short[][][] blockData = new short[16][height][16];
 
         for (short[][] array2d : blockData)
         {
@@ -21,5 +21,37 @@ public class BaseBenchmark
         }
 
         return blockData;
+    }
+
+    
+
+    public class Benchmark
+    {
+        private long _startTime;
+        private float _averageTotal = 0f;
+        private int _amount = 0;
+        protected void StartBenchmark()
+        {
+            _startTime = System.nanoTime();
+        }
+
+        protected float StopBenchmark()
+        {
+            long endTime = System.nanoTime();
+
+            // Calculate and print the execution time
+            long executionTimeInNanos = endTime - _startTime;
+            float executionTimeInMillis = (float) executionTimeInNanos / 1_000_000;
+
+            _averageTotal += executionTimeInMillis;
+            ++_amount;
+
+            return executionTimeInMillis;
+        }
+
+        protected float GetAverageMs()
+        {
+            return _averageTotal /= _amount;
+        }
     }
 }
--- benchmark/Json.java
+++ benchmark/Json.java
@@ -1,4 +1,6 @@
 package benchmark;
+//import java.util.concurrent.TimeUnit;
+
 import networking.Server;
 import packets.JsonPacket;
 
@@ -12,11 +14,38 @@ public class Json extends BaseBenchmark
 
     public void FullChunk()
     {
-        JsonPacket packet = new JsonPacket();
-        packet.SetInt16((short)20, "PacketId");
-        packet.SetIVec2(0, 0, "Position");
-        packet.SetUChar((byte)0, "Biome");
-        packet.SetChunk(GenerateRandomChunk(), "Chunk");
-        _server.SendAllUDP(packet.GetData());
+        var chunk = GenerateRandomChunk(48);
+        JsonPacket[] packets = new JsonPacket[8];
+
+        Benchmark allPackets = new Benchmark();
+        int length = 0;
+        for (int x = 0; x < 5000; ++x)
+        {
+            length = 0;
+            allPackets.StartBenchmark();
+            for (int i = 0; i < packets.length; ++i)
+            {
+                JsonPacket packet = new JsonPacket();
+                packet.SetInt16((short)20, "PacketId");
+                packet.SetIVec2(0, 0, "Position");
+                packet.SetUChar((byte)0, "Biome");
+                packet.SetChunk(chunk, "Chunk");
+                var data = packet.GetData();
+                length += data.length();
+            }
+            allPackets.StopBenchmark();
+
+            /*try
+            {
+                TimeUnit.MILLISECONDS.sleep(100);
+                
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }*/
+        }
+        
+        System.out.println("Total packets length: " + length);
+        System.out.println("Execution time average: " + allPackets.GetAverageMs() + "ms");
     }
 }
